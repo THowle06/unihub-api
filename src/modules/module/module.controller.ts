92399272100540de6c9f1c80380080aa1942a96c
Tyler Howle <t.howle06@outlook.com>
2026-07-27 10:19:14 +0100

    feat: implement module retrieval controller functions

--- src/modules/module/module.controller.ts
+++ src/modules/module/module.controller.ts
@@ -5,6 +5,10 @@ import * as moduleService from "./module.service";
 import { createModuleSchema, moduleResponseSchema } from "./module.types";
 import { StatusCodes } from "http-status-codes";
 
+type ModuleParams = {
+  id: string;
+};
+
 export async function createModule(req: Request, res: Response) {
   try {
     if (!req.user) {
@@ -42,6 +46,66 @@ export async function createModule(req: Request, res: Response) {
 
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       message: "Internal server error",
+    });
+  }
+}
+
+export async function getModules(req: Request, res: Response) {
+  try {
+    if (!req.user) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const modules = await moduleService.getModules(req.user.id);
+
+    const response = modules.map((module) =>
+      moduleResponseSchema.parse({
+        ...module,
+        createdAt: module.createdAt.toISOString(),
+        updatedAt: module.updatedAt.toISOString(),
+      }),
+    );
+
+    return res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    console.error(error);
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
+export async function getModuleById(req: Request<ModuleParams>, res: Response) {
+  try {
+    if (!req.user) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "Unauthorized",
+      });
+    }
+
+    const module = await moduleService.getModuleById(req.user.id, req.params.id);
+
+    if (!module) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        message: "Module not found",
+      });
+    }
+
+    const response = moduleResponseSchema.parse({
+      ...module,
+      createdAt: module.createdAt.toISOString(),
+      updatedAt: module.updatedAt.toISOString(),
+    });
+
+    return res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    console.error(error);
+
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      message: "Internal Server Error",
     });
   }
 }
